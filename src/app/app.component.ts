3ae0cd23805a5586c0bf38661577d52ff97adc52
unknown <unenes.viejos@gmail.com>
2020-12-08 21:28:30 -0600

    Uso de la interpolacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Projecto-Cero';
+  // Uso de interpolacion
+  Nombre = 'Bryan Orozco';
+
+  constructor(){
+    // Actualizar la variable Nombre y poner en MIGUEL
+    setInterval( ()=> this.Nombre = 'Miguel', 3000 );
+  }
+
+  getSuma(numero1: number, numero2: number)
+  {
+    return numero1 + numero2
+  }
 }
